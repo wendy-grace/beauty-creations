245635fe84fcafacaf0ae98ee0a805e4a27b5ffc
Charles Kamiri <charleskamiri6@gmail.com>
2024-02-29 17:34:42 +0300

    feat: Add design for admin-page

--- js/admin.js
+++ js/admin.js
@@ -3,4 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('beautyCreationsAdmin') === null) {
         window.location.href = '/pages/admin-sign-in.html';
     }
+
+
+    const reservations = document.getElementById('reservations');
+
+    // Fetch reservations
+    fetch('http://localhost:3000/api/reservations')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(reservation => {
+
+            const reservationHTML = `
+            <div className='reservation'>
+                <div className='service'>${reservation.service}</div>
+                <div className='name'>${reservation.firstname} ${reservation.lastname}</div>
+
+            </div>
+            `
+
+            reservations.innerHTML += reservationHTML;
+        })
+    })
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const db = mysql.createConnection({
 	host: "localhost",
@@ -20,6 +21,8 @@ db.connect((err) => {
 const app = express(); // Use express to create the app
 
 app.use(cors());
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.get("/", (req, res) => {
 	res.writeHead(200, { "Content-Type": "text/html" });
@@ -45,10 +48,16 @@ app.get("/api/reservations", (req, res) => {
 
 
 // Admin sign in
-app.get("/api/admin-sign-in", (req, res) => {
-	const { username, password } = req.query;
+app.post("/api/admin-sign-in", (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	// Log username and password to console
+	console.log(`username: ${username}, password: ${password}`);
+
+
 	db.query(
-		`SELECT * from admin WHERE username = '${username}' AND password = '${password}'`,
+		`SELECT * from admins WHERE username = '${username}' AND password = '${password}'`,
 		(err, result) => {
 			if (err) {
 				throw err;
@@ -57,6 +66,18 @@ app.get("/api/admin-sign-in", (req, res) => {
 			res.end(JSON.stringify(result));
 		}
 	);
+});
+
+// Get admins
+app.get("/api/admins", (req, res) => {
+	console.log("Getting admins");
+	db.query("SELECT * from admins", (err, result) => {
+		if (err) {
+			throw err;
+		}
+		res.writeHead(200, { "Content-Type": "application/json" });
+		res.end(JSON.stringify(result));
+	});
 });
 
 const PORT = process.env.PORT || 3000;
